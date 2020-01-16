f6ab79c8afae16299e158f0113fe5a39cbaa008b
Armel E <armelem@ausiteodit.com>
2020-01-16 21:59:23 +0100

    version serveur

--- src/app/stokcs/stokcs.page.ts
+++ src/app/stokcs/stokcs.page.ts
@@ -1,7 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { UtilsService } from "../services/utils.service";
+import { CategorieService } from "../services/categorie.service";
 import { ProduitService } from "../services/produit.service";
-import { UsersService } from "../services/users.service";
+import { TableService } from './../services/table.service';
+import {error} from 'util';
+import { CommandeService } from "../services/commande.service";
+import { NavigationExtras } from '@angular/router';
+import { NavController } from '@ionic/angular';
+import { ActivatedRoute } from '@angular/router';
+import * as $ from 'jquery';
 
 @Component({
   selector: 'app-stokcs',
@@ -10,108 +18,108 @@ import { UsersService } from "../services/users.service";
 })
 export class StokcsPage implements OnInit {
 
+  private workMoney: any = 0;
+  private time:String="";
+  private categories: any;
+  private tableShop : any;
+  private produits : any;
+  private cat_id:any;
+  private selected:number=-1;
+  private cat_name:any;
+   constructor(     private prod : ProduitService,private catService: CategorieService,public utilService:UtilsService,public navCtrl: NavController,private commandeService:CommandeService,private router: Router,private route: ActivatedRoute, private tableService: TableService){
 
-	public searchInput:string;
+    }
 
-	public commandes:Array<any>;
+  goToAnoterPage(url){
+    this.router.navigateByUrl(url);
+  }
+  ionViewDidEnter() {
+    this.initCategory();
+    this.dateInit();
+    this.tableService.getAllOfMyShop().then(datas=>{
+        this.tableShop = datas;
+            console.log(datas);
+    });
+  }
 
-	public commandePages:Array<Array<any>>;
+  showListCategory(){
+    if($("#stock-produits").hasClass("hide-list-category")){
+      $("#stock-produits").removeClass("hide-list-category");
+    }
+  }
+  hideListCategory(){
+    if(!$("#stock-produits").hasClass("hide-list-category")){
+      $("#stock-produits").addClass("hide-list-category");
+    }
+  }
+  openOder(order_id){
+    let navigationExtras: NavigationExtras = {
+        queryParams: {
+            order_id: order_id
+        }
+    };
+    this.navCtrl.navigateForward(['/commandes/new'], navigationExtras);
+  }
+  dateInit(){
+    var date = new Date();
 
-	public produits:Array<any>;
+    var seconds = date.getSeconds();
+    var minutes = date.getMinutes();
+    var hour = date.getHours();
+    this.time = hour+" : "+minutes;
+    setInterval(
+      function(){
+         seconds = date.getSeconds();
+         minutes = date.getMinutes();
+           hour = date.getHours();
+        this.time = hour+" : "+minutes;
+      },60000
+    );
+  }
+  removeText(text){
+    if(text=='' || text == 'undefined')
+      return;
+    return text.replace("index.php/","");
+  }
+  initCategory(){
 
-	public produitsPages:any;
+    let utilService = this.utilService;
 
-	public showList:boolean = false;
+    this.utilService.presentLoading("Chargement de Categories");
+    this.catService.getAllByUser().then(datas=>{
+            this.categories = datas;
+            utilService.dismissLoading();
+        });
+    
+  }
+  openDetailProduct(indice){
+    this.selected = this.produits[indice];
+  }
+  initItems(event=null,cat_id=null,cat_name) {
+    if(cat_id){
+      this.cat_id = cat_id;
+      this.cat_name = cat_name;
+    }
 
+    let utilService = this.utilService;
 
-  constructor(
-      private router: Router,
-      private prod: ProduitService,
-      private user: UsersService
-      ) { }
-
+    if(!event)
+      this.utilService.presentLoading("Chargement des Produits...") ;
+    this.prod.getProductByCategory(this.cat_id).then(
+      datas =>{
+        this.produits = datas;
+        this.hideListCategory();
+        if(!event)
+          utilService.dismissLoading();
+        else
+          event.target.complete();
+      },error=>{
+        console.log(error);
+      }
+    );
+  }
   ngOnInit() {
-  	//this.getProduct();
-  	/*this.produits = this.prod.getAll();
-  	   let commandesList = ["Fruit","Glace","Boissons","légumes","Céréales","féculents","Produits","Viande","poisson","œuf","Sucre","Corps gras"];
-    this.commandes = [];
-    for (var i = 1; i <= commandesList.length ; ++i) {
-      this.commandes.push({id: i,name:commandesList[i-1],statusFilter: false});
-      
-    }
-  	this.commandePages = this.convertArrayToPagible(this.commandes, 6);
-  	this.produitsPages = this.convertArrayToPagible(this.produits, 3);*/
-
-  	//this.produitsPages = this.convertArrayToPagible(this.produits, 3);
-    //this.setToCard();
-
+   
   }
-    ionViewWillEnter(){
-        this.getProduct();
-        // this.prod.ProductResto().then(produitResto=>{
-        //         this.produits = produitResto;
-        //     },error=>{
-        //         console.log(error);
-        //     }
-        // );
-    }
-
-
-  public rechercher() {}
-  getProduct(){
-      let datas = localStorage.getItem('produits');
-      //this.produits = JSON.parse(datas);
-  }
-
-    public setToList(){
-        console.log('show list');
-        this.showList = true;
-    }
-
-    public setToCard(){
-        console.log('show cart');
-        this.showList = false;
-    }
-  /*public convertArrayToPagible(datas: Array<any>, pagesize: number):Array<Array<any>>{
-  	let result:Array<Array<any>> = [];
-  	for (var i = 0; i < (datas.length / pagesize); ++i) {
-  		if (i!=((datas.length / pagesize)-1))
-  			result.push(datas.slice(i*pagesize, (i+1)*pagesize));
-  		else
-  			result.push(datas.slice(i*pagesize, datas.length));
-  		
-  	}
-  	return result;
-  }
-
-  public addOrRemoveCategorieToFilter(line, col){
-  	if (this.commandePages[line][col].statusFilter) {
-	  	this.commandePages[line][col].statusFilter = false;
-	  	this.commandes[line*6 + col].statusFilter = false;
-  	}else{
-	  	this.commandePages[line][col].statusFilter = true;
-	  	this.commandes[line*6 + col].statusFilter = true;
-  	}
-
-  }
-  */
-
-  public editer(produit){
-  	this.router.navigate(["/stokcs/edit/"+produit.id],{queryParams:produit});
-  } 
-
-  public details(produit){
-    this.router.navigate(["/stokcs/details/"+produit.id],{queryParams:produit});
-  }
-
-  public supprimer(produit:any){
-  	return null;
-  }
-  categorieByID(id){
-    // console.log(this.prod.categorie(id));
-  }
- public ajoutGlobal(){
-        this.router.navigate(["/stokcs/add-produit"]);
- }
-
 }
+
